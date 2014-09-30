9c9c88c0622a91fe076c518b10e6d969455a99b5
iCHAIT <cgupta319@gmail.com>
2014-09-30 22:01:16 +0530

    views.py changed

--- mymtero/metro/views.py
+++ mymtero/metro/views.py
@@ -1,22 +1,41 @@
 from django.shortcuts import render_to_response, RequestContext
 
-from metro.models import station
+from .forms import EmailForm
 
-from metro.models import facility
-
-from metro.models import places
-
-from metro.models import junction
+from .models import Join
 
 # Create your views here.
 
 def home(request):
+    
     return render_to_response('index.html')
 
 
 def about(request):
-    return render_to_response('about.html')
+    #print request.POST["email"]
+    
+    #This is using regular django forms
+    
+    form = EmailForm(request.POST or None)#validation
+        if form.is_valid():
+            email = form.cleaned_data['email']
+                new_join, created = Join.objects.get_or_create(email=email)
+                print new_join, created
+                print new_join.timestamp
+                if created:
+                    print "This obj was created"
+    
+    
+    
+        #This is using models form
+        
+        #form = JoinForm(request.POST or None)
+        
+        
+        context = {"form": form}
+    return render_to_response('about.html', context, context_instance=RequestContext(request))
 
+#return render_to_response('about.html')
 
 def info(request):
     return render_to_response('info.html')
