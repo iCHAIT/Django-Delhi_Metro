3a9afc27bfe89c22405dc81ec3f3c3ab2d2d8a80
iCHAIT <cgupta319@gmail.com>
2014-09-30 21:59:15 +0530

    models.py changed

--- mymtero/metro/models.py
+++ mymtero/metro/models.py
@@ -1,35 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class station(models.Model):
-    sno = models.IntegerField()
-    sname = models.CharField(max_length = 50)
-    line = models.CharField(max_length = 15)
-    pincode = models.IntegerField()
-    grade = models.CharField(max_length = 15)
-    #pk = CompositeField(('sname', 'line'))
-#thing.pk.primary_key= True
 
-class facility(models.Model):
+class stationinfo(models.Model):
     sname = models.CharField(max_length = 50,primary_key = True)
     washroom = models.CharField(max_length = 5)
-    elevator = models.CharField(max_length = 5)
     parking = models.CharField(max_length = 5)
+    elevator = models.CharField(max_length = 5)
     opening_date = models.DateField()
     contact = models.BigIntegerField()
-    sname = models.ForeignKey(station)
+    def __str__(self):
+        return self.sname
+
+class station(models.Model):
+    sno = models.IntegerField()
+    statname = models.CharField(max_length = 50,null = False)
+    line = models.CharField(max_length = 15,null = False)
+    pincode = models.BigIntegerField()
+    grade = models.CharField(max_length = 15)
+    sname = models.ForeignKey('stationinfo',to_field = 'sname')
+    class Meta:
+        unique_together = (('statname','line'))
+    def __str__(self):
+        return self.statname
+
 
 class places(models.Model):
-    sname = models.CharField(max_length = 50,primary_key = True)
-    place1 = models.CharField(max_length = 50)
-    place2 = models.CharField(max_length = 50)
-    place3 = models.CharField(max_length = 50)
-    sname = models.ForeignKey(station)
+    statname = models.CharField(max_length = 50)
+    place = models.CharField(max_length = 75)
+    sname = models.ForeignKey('stationinfo',to_field = 'sname')
+    def __str__(self):
+        return self.statname
+
 
 class junction(models.Model):
-    sname = models.CharField(max_length = 50,primary_key = True)
+    statname = models.CharField(max_length = 50,primary_key = True)
     line1 = models.CharField(max_length = 15)
     line2 = models.CharField(max_length = 15)
-    sname = models.ForeignKey(station)
+    sname = models.ForeignKey('stationinfo',to_field = 'sname')
+    def __str__(self):
+        return self.statname
+
+
+class review(models.Model):
+    sname = models.CharField(max_length = 50)
+    title = models.CharField(max_length = 50)
+    timest = models.DateTimeField()
+    author = models.CharField(max_length = 50)
+    bodytext = models.TextField()
+    def __str__(self):
+        return self.sname
+
+class Join(models.Model):
+    email = models.EmailField()
+    timestamp = models.DateTimeField()
+    updated = models.DateTimeField()
+
+
+
+
+
 
