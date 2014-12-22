612b5e0fb3d22a8a5f89f203ba73e5e7825fdaf7
iCHAIT <cgupta319@gmail.com>
2014-12-23 01:57:07 +0530

    w

--- mymtero/mymtero/settings/base.py
+++ mymtero/mymtero/settings/base.py
@@ -99,7 +99,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
