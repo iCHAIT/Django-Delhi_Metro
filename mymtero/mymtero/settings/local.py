6f71f07cd5d2d3c9d4e32d916d988b015ae1eaad
iCHAIT <cgupta319@gmail.com>
2014-12-23 13:33:42 +0530

    set

--- mymtero/mymtero/settings/local.py
+++ mymtero/mymtero/settings/local.py
@@ -99,7 +99,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = os.path.join(mymetro, 'static')
+STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
