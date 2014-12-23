0ca27d488cfbeee93afa6f4fe8cf110827dff7a4
iCHAIT <cgupta319@gmail.com>
2014-12-24 01:06:19 +0530

    repo synced

--- mymtero/Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn mymtero.wsgi --log-file -
--- mymtero/mymtero/static/js/car.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    //Handles menu drop down
-    $('.dropdown-menu').find('form').click(function (e) {
-        e.stopPropagation();
-    });
-});
