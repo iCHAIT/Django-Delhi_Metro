c5dcc1bb67890458f692e25ed4692e525a42dd0a
iCHAIT <cgupta319@gmail.com>
2015-05-26 22:35:42 +0530

    PEP8 changes

--- mymtero/mymtero/settings.py
+++ mymtero/mymtero/settings.py
@@ -30,29 +30,29 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-                  'django.contrib.admin',
-                  'django.contrib.auth',
-                  'django.contrib.contenttypes',
-                  'django.contrib.sessions',
-                  'django.contrib.messages',
-                  'django.contrib.staticfiles',
-                  'metro',
-                  'django.contrib.admin',
-                  'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'metro',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
 
 
 
 
-                  )
+)
 
 MIDDLEWARE_CLASSES = (
-                      'django.contrib.sessions.middleware.SessionMiddleware',
-                      'django.middleware.common.CommonMiddleware',
-                      'django.middleware.csrf.CsrfViewMiddleware',
-                      'django.contrib.auth.middleware.AuthenticationMiddleware',
-                      'django.contrib.messages.middleware.MessageMiddleware',
-                      'django.middleware.clickjacking.XFrameOptionsMiddleware',
-                      )
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
 
 ROOT_URLCONF = 'mymtero.urls'
 
@@ -79,7 +79,7 @@ DATABASES = {
 
 
 
-        }
+    }
 }
 
 # Internationalization
@@ -106,28 +106,26 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
 
-                    "metro/static",
+    "metro/static",
 
 
-                    )
+)
 
 
 STATICFILES_FINDERS = (
 
-                       'django.contrib.staticfiles.finders.FileSystemFinder',
-                       'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 
 
-                       )
+)
 
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-                               "django.contrib.auth.context_processors.auth",
-                               "django.core.context_processors.debug",
-                               "django.core.context_processors.i18n",
-                               "django.core.context_processors.media",
-                               "django.core.context_processors.static",
-                               "django.core.context_processors.csrf",
-                               )
-
-
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.csrf",
+)
--- mymtero/mymtero/urls.py
+++ mymtero/mymtero/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from django.conf import settings
-
-from django.conf.urls.static import static
-
-#keep next 2 lines uncommented to enable admin
+# keep next 2 lines uncommented to enable admin
 
 from django.contrib import admin
 admin.autodiscover()
@@ -13,46 +9,48 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        # Examples:
                        url(r'^$', 'metro.views.home', name='home'),
-                       
+
                        url(r'^home/', 'metro.views.home', name='home'),
-                       
-                       url(r'^directions/', 'metro.views.directions', name='directions'),
-                       
-                       url(r'^directions2/', 'metro.views.directions2', name='directions2'),
-                       
+
+                       url(r'^directions/', 'metro.views.directions',
+                           name='directions'),
+
+                       url(r'^directions2/', 'metro.views.directions2',
+                           name='directions2'),
+
                        url(r'^info/', 'metro.views.info', name='info'),
-                       
+
                        url(r'^info2/', 'metro.views.info2', name='info2'),
-                       
-                       url(r'^nearest/', 'metro.views.nearest', name='nearest'),
-                       
-                       url(r'^nearest2/', 'metro.views.nearest2', name='nearest2'),
 
-                       url(r'^nearest3/', 'metro.views.nearest3', name='nearest3'),
-                       
-                       url(r'^review/','metro.views.review', name='review'),
-                       
-                       url(r'^review2/','metro.views.review2', name='review2'),
+                       url(r'^nearest/', 'metro.views.nearest',
+                           name='nearest'),
 
-                       
-                       url(r'^review3/','metro.views.review3', name='review3'),
-                       
-                       
+                       url(r'^nearest2/', 'metro.views.nearest2',
+                           name='nearest2'),
+
+                       url(r'^nearest3/', 'metro.views.nearest3',
+                           name='nearest3'),
+
+                       url(r'^review/', 'metro.views.review', name='review'),
+
+                       url(r'^review2/', 'metro.views.review2',
+                           name='review2'),
+
+
+                       url(r'^review3/', 'metro.views.review3',
+                           name='review3'),
+
+
                        url(r'^about/', 'metro.views.about', name='about'),
-                       
-                       
-                       #keep the next 2 lines uncommented to enable admin
-                       
-                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-                       
+
+
+                       # keep the next 2 lines uncommented to enable admin
+
+                       url(r'^admin/doc/',
+                           include('django.contrib.admindocs.urls')),
+
                        url(r'^admin/', include(admin.site.urls)),
                        )
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns += staticfiles_urlpatterns()
-
-
-
-
-
-
