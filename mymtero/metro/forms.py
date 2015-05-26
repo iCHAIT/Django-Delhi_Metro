27cfd9282288327963ee26388b381a29398fe064
iCHAIT <cgupta319@gmail.com>
2015-05-26 22:39:48 +0530

    PEP8 changes

--- mymtero/metro/forms.py
+++ mymtero/metro/forms.py
@@ -15,50 +15,47 @@ from .models import rev1
 from .models import rev2
 
 
-
-
 class dirForm(forms.Form):
     cursor = connection.cursor()
     cursor.execute("select sname,sname from metro_stationinfo")
     CHOICES = cursor.fetchall()
-    source = forms.ChoiceField(choices = CHOICES)
-    dest = forms.ChoiceField(choices = CHOICES)
+    source = forms.ChoiceField(choices=CHOICES)
+    dest = forms.ChoiceField(choices=CHOICES)
 
 
 class infoForm(forms.Form):
     cursor = connection.cursor()
     cursor.execute("select sname,sname from metro_stationinfo")
     CHOICES = cursor.fetchall()
-    sname = forms.ChoiceField(choices = CHOICES)
+    sname = forms.ChoiceField(choices=CHOICES)
 
 
 class near1Form(forms.Form):
     cursor = connection.cursor()
     cursor.execute("select place,place from metro_places")
     CHOICES = cursor.fetchall()
-    place = forms.ChoiceField(choices = CHOICES)
+    place = forms.ChoiceField(choices=CHOICES)
 
 
 class near2Form(forms.Form):
     cursor = connection.cursor()
     cursor.execute("select pincode,pincode from metro_stationinfo")
     CHOICES = cursor.fetchall()
-    pin = forms.ChoiceField(choices = CHOICES)
+    pin = forms.ChoiceField(choices=CHOICES)
 
 
 class rev1Form(forms.Form):
     cursor = connection.cursor()
     cursor.execute("select sname,sname from metro_stationinfo")
     CHOICES = cursor.fetchall()
-    sname = forms.ChoiceField(choices = CHOICES)
+    sname = forms.ChoiceField(choices=CHOICES)
+
 
 class rev2Form(forms.Form):
     cursor = connection.cursor()
     cursor.execute("select sname,sname from metro_stationinfo")
     CHOICES = cursor.fetchall()
-    sname = forms.ChoiceField(choices = CHOICES)
+    sname = forms.ChoiceField(choices=CHOICES)
     title = forms.CharField()
     bodytext = forms.CharField(widget=forms.Textarea)
     author = forms.CharField()
-
-
--- mymtero/metro/views.py
+++ mymtero/metro/views.py
@@ -32,7 +32,7 @@ def home(request):
 
 def directions(request):
     form = dirForm(request.POST or None)
-    return render_to_response('directions.html', {'form': form}, context_instance = RequestContext(request))
+    return render_to_response('directions.html', {'form': form}, context_instance=RequestContext(request))
 
 
 def directions2(request):
@@ -41,16 +41,14 @@ def directions2(request):
         source = form.cleaned_data['source']
         dest = form.cleaned_data['dest']
         cursor = connection.cursor()
-        cursor.execute("call find_path(%s,%s)",[source,dest])
+        cursor.execute("call find_path(%s,%s)", [source, dest])
         data = cursor.fetchall()
-    return render_to_response( 'directions2.html', {'data': data, 'source': source, 'dest': dest})
-
+    return render_to_response('directions2.html', {'data': data, 'source': source, 'dest': dest})
 
 
 def info(request):
     form = infoForm(request.POST or None)
-    return render_to_response('info.html', {'form': form}, context_instance = RequestContext(request))
-
+    return render_to_response('info.html', {'form': form}, context_instance=RequestContext(request))
 
 
 def info2(request):
@@ -58,20 +56,21 @@ def info2(request):
     if form.is_valid():
         sname = form.cleaned_data['sname']
         cursor = connection.cursor()
-        cursor.execute("SELECT * from metro_stationinfo where sname = %s",[sname])
+        cursor.execute(
+            "SELECT * from metro_stationinfo where sname = %s", [sname])
         infor = cursor.fetchone()
-        cursor.execute("SELECT * from metro_station where sname =%s",[sname])
+        cursor.execute("SELECT * from metro_station where sname =%s", [sname])
         inform = cursor.fetchall()
-        cursor.execute("SELECT place from metro_places where sname =%s",[sname])
+        cursor.execute(
+            "SELECT place from metro_places where sname =%s", [sname])
         informa = cursor.fetchall()
-        return render_to_response('info2.html',{'infor': infor,'inform': inform,'informa': informa})
-
+        return render_to_response('info2.html', {'infor': infor, 'inform': inform, 'informa': informa})
 
 
 def nearest(request):
     form1 = near1Form(request.POST or None)
     form2 = near2Form(request.POST or None)
-    return render_to_response('nearest.html', {'form1': form1,'form2':form2}, context_instance = RequestContext(request))
+    return render_to_response('nearest.html', {'form1': form1, 'form2': form2}, context_instance=RequestContext(request))
 
 
 def nearest2(request):
@@ -79,16 +78,19 @@ def nearest2(request):
     cursor = connection.cursor()
     if form1.is_valid():
         place = form1.cleaned_data['place']
-        cursor.execute("SELECT sname from metro_places where place =%s",[place])
+        cursor.execute(
+            "SELECT sname from metro_places where place =%s", [place])
         data1 = cursor.fetchall()
-        return render_to_response('nearest2.html', {'data1':data1})
+        return render_to_response('nearest2.html', {'data1': data1})
+
 
 def nearest3(request):
     form2 = near2Form(request.POST or None)
     if form2.is_valid():
         pin = form2.cleaned_data['pin']
         cursor = connection.cursor()
-        cursor.execute("SELECT sname from metro_stationinfo where pincode =%s",[pin])
+        cursor.execute(
+            "SELECT sname from metro_stationinfo where pincode =%s", [pin])
         data2 = cursor.fetchall()
     return render_to_response('nearest2.html', {'data2': data2})
 
@@ -96,7 +98,7 @@ def nearest3(request):
 def review(request):
     form1 = rev1Form(request.POST or None)
     form2 = rev2Form(request.POST or None)
-    return render_to_response('review.html', {'form1': form1, 'form2': form2}, context_instance = RequestContext(request))
+    return render_to_response('review.html', {'form1': form1, 'form2': form2}, context_instance=RequestContext(request))
 
 
 def review2(request):
@@ -104,9 +106,11 @@ def review2(request):
     if form1.is_valid():
         sname = form1.cleaned_data['sname']
         cursor = connection.cursor()
-        cursor.execute("SELECT sname,title,author,bodytext from metro_review where approved = 'Yes'")
+        cursor.execute(
+            "SELECT sname,title,author,bodytext from metro_review where approved = 'Yes'")
         data = cursor.fetchall()
     return render_to_response('review2.html', {'data': data})
+
 
 def review3(request):
     form2 = rev2Form(request.POST or None)
@@ -116,7 +120,8 @@ def review3(request):
         bodytext = form2.cleaned_data['bodytext']
         author = form2.cleaned_data['author']
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO metro_review (sname,title,author,bodytext,timest,approved) VALUES (%s,%s,%s,%s,now(),'no')",[sname,title,author,bodytext])
+        cursor.execute("INSERT INTO metro_review (sname,title,author,bodytext,timest,approved) VALUES (%s,%s,%s,%s,now(),'no')", [
+                       sname, title, author, bodytext])
         cursor.execute('COMMIT')
     data = "Review has been added. It will be displayed once approved by the admin."
     return render_to_response('boilerplate.html', {'data': data})
@@ -124,6 +129,3 @@ def review3(request):
 
 def about(request):
     return render_to_response('about.html')
-
-
-
